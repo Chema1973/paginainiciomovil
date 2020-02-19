c9be8e8e43d54f255ef0f29072570b502c155c59
Chema1973 <chema.pairet@gmail.com>
2020-02-19 18:12:35 +0100

    Resize 2

--- src/components/ImgHome.js
+++ src/components/ImgHome.js
@@ -34,10 +34,10 @@ class ImgHome extends React.Component{
 
         // console.log('componentDidMount');
         window.addEventListener('resize', this.updateDimensions.bind(this));
-        const oHeight = document.documentElement.clientHeight|| document.body.clientHeight || window.innerHeight;
-        const oWidth = document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth;
+        // const oHeight = document.documentElement.clientHeight|| document.body.clientHeight || window.innerHeight;
+        // const oWidth = document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth;
 
-        if (oHeight > oWidth) {
+        /* if (oHeight > oWidth) {
             // console.log('Vertical - Es más alto');
             this.setState({
                 widthScreen: undefined,
@@ -53,13 +53,13 @@ class ImgHome extends React.Component{
                 widthImg: (document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth) - data.dataConf.marginbody, // oImgSize.width,  //  window.innerWidth - data.dataConf.marginbody, // document.getElementById('imgHome').offsetWidth,
                 heightImg: (document.documentElement.clientHeight|| document.body.clientHeight || window.innerHeight) - data.dataConf.marginbody //  oImgSize.height // window.innerHeight - data.dataConf.marginbody //  document.getElementById('imgHome').offsetHeight
             });
-        }
-        /* this.setState({
+        } */
+        this.setState({
             widthScreen: document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth,
             heightScreen: document.documentElement.clientHeight|| document.body.clientHeight || window.innerHeight,
             widthImg: (document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth) - data.dataConf.marginbody, // oImgSize.width,  //  window.innerWidth - data.dataConf.marginbody, // document.getElementById('imgHome').offsetWidth,
             heightImg: (document.documentElement.clientHeight|| document.body.clientHeight || window.innerHeight) - data.dataConf.marginbody //  oImgSize.height // window.innerHeight - data.dataConf.marginbody //  document.getElementById('imgHome').offsetHeight
-        }); */
+        });
         this.context.widthImg = this.state.widthImg;
         this.context.heightImg = this.state.heightImg;
         this.context.widthScreen = this.state.widthScreen;
@@ -91,7 +91,7 @@ class ImgHome extends React.Component{
         console.log('updateDimensions')
         const oHeight = document.documentElement.clientHeight|| document.body.clientHeight || window.innerHeight;
         const oWidth = document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth;
-        if (oHeight > oWidth) {
+        /* if (oHeight > oWidth) {
             // console.log('Vertical - Es más alto');
             this.setState({
                 widthScreen: undefined,
@@ -107,27 +107,26 @@ class ImgHome extends React.Component{
                 widthImg: this.getWindowWidth() - data.dataConf.marginbody,
                 heightImg: this.getWindowHeight() - data.dataConf.marginbody
             });
-        }
-        /* this.setState({
+        } */
+        this.setState({
             widthScreen: this.getWindowWidth(),
             heightScreen: this.getWindowHeight(),
             widthImg: this.getWindowWidth() - data.dataConf.marginbody,
             heightImg: this.getWindowHeight() - data.dataConf.marginbody
-        }); */
+        });
         this.context.widthScreen = this.state.widthScreen;
         this.context.heightScreen = this.state.heightScreen;
         this.context.widthImg = this.state.widthImg;
         this.context.heightImg = this.state.heightImg;
         this.forceUpdate();
-/*         console.log(window.innerWidth + ' || ' + document.documentElement.clientWidth + ' || ' + document.body.clientWidth,);
-        console.log('Chema::Punto De Control::updateDimension');
-        console.log(this.context.widthImg + ' - ' + this.context.heightImg);
-        console.log(this.context.widthScreen + ' - ' + this.context.heightScreen); */
         // El problema que se estaba dando es que al Maximizar o Minimizar la ventana
         // entraba aquí, lanzaba el render y luego actulizaba las dimensiones.
         // Esto provocaba un error en el tamaño de la imagen, que mantenía las previas
 
-
+/*      console.log(window.innerWidth + ' || ' + document.documentElement.clientWidth + ' || ' + document.body.clientWidth,);
+        console.log('Chema::Punto De Control::updateDimension');
+        console.log(this.context.widthImg + ' - ' + this.context.heightImg);
+        console.log(this.context.widthScreen + ' - ' + this.context.heightScreen); */
       };
       
       oPrueba = (evt) => {
@@ -141,10 +140,7 @@ class ImgHome extends React.Component{
                 window.screen.height + ' - ' + 
                 window.screen.width; */
 
-                var sContent = this.context.widthScreen + ' - ' +
-                this.context.heightScreen + ' - ' + 
-                window.screen.height + ' - ' + 
-                window.screen.width;
+                
         if (this.context.widthImg === 0 || this.context.heightImg === 0) {
             return (
                 <img
@@ -161,12 +157,7 @@ class ImgHome extends React.Component{
         return (
             
             <div style={{backGroundColor:'black'}}>
-                <textarea rows = "5" cols = "50" 
-                    className="textAreaPrueba"
-                    onChange={this.oPrueba}
-                value={sContent}>
-                
-                </textarea>
+               
             <img
             style= {{flex:1 , width: this.state.widthScreen, height: this.state.heightScreen}}  
                 ref={this.refImgCentral}
@@ -194,3 +185,13 @@ export default ImgHome;
 
 // style={{display:"flex"}} 
 // --> Para la imagen
+/* var sContent = this.context.widthScreen + ' - ' +
+                this.context.heightScreen + ' - ' + 
+                window.screen.height + ' - ' + 
+                window.screen.width;
+<textarea rows = "5" cols = "50" 
+className="textAreaPrueba"
+onChange={this.oPrueba}
+value={sContent}>
+
+</textarea> */
